a724d93eb57e43533c694a65d4698e9799a4b299
Eduardo Stefanello <eduardo@stefanello.cc>
2024-09-09 22:47:40 -0300

    -src

--- src/admin_links/apps.py
+++ src/admin_links/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AdminLinksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.admin_links'
+    name = 'admin_links'
--- src/summary_admin/apps.py
+++ src/summary_admin/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoSummaryAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.summary_admin'
+    name = 'summary_admin'
