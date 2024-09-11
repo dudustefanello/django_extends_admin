dea98c2981ae68963ad8253951c63015b9bc5dbc
Eduardo Stefanello <eduardo@stefanello.cc>
2024-09-10 21:24:55 -0300

    migrate

--- src/summary_admin/admin.py
+++ src/summary_admin/admin.py
@@ -5,7 +5,7 @@ class SummaryAdmin(admin.ModelAdmin):
     change_list_template = 'summary_admin/summary_change_list.html'
 
     def get_summary(self, request, queryset=None):
-        """ :rtype: {'field_name': Decimal}
+        """ :rtype: {'header_text': Decimal}
         """
         return None
 
@@ -18,3 +18,27 @@ class SummaryAdmin(admin.ModelAdmin):
         except AttributeError:
             pass
         return view
+
+
+class SummaryTabularInline(admin.TabularInline):
+    template = 'summary_admin/summary_tabular.html'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.queryset = None
+
+    def get_queryset(self, request):
+        if self.queryset:
+            return self.queryset
+        return super().get_queryset(request)
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj, **kwargs)
+        self.queryset = self.get_queryset(request)
+        setattr(formset, 'summary', self.get_summary(request, self.queryset.filter(**{formset.fk.name: obj})))
+        return formset
+
+    def get_summary(self, request, queryset=None):
+        """ :rtype: {'field_name': Decimal}
+        """
+        return None
